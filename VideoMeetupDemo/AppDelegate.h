e35acd4451f3fa4fd0b9243edff992311002dce4
Barry Ezell <barrye@gmail.com>
2015-06-23 10:30:23 -0400

    Initial Commit

---
+++ VideoMeetupDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoMeetupDemo
+//
+//  Created by BARRY EZELL on 6/23/15.
+//  Copyright (c) 2015 Barry Ezell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoMeetupDemo/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  VideoMeetupDemo
+//
+//  Created by BARRY EZELL on 6/23/15.
+//  Copyright (c) 2015 Barry Ezell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
